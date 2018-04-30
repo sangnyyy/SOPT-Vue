516f5bd22efad60fbb86a66884a0069cdaa6d714
SML <dltkdals2202@gmail.com>
2018-04-30 17:59:25 +0900

    add : personal practice directory
    practice css transition file.

---
+++ personal/transition/src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div id="app">
+    <button-fade></button-fade>
+    
+  </div>
+</template>
+
+<script>
+
+import ButtonFade from "./components/ButtonFade"
+
+export default {
+  name: 'App',
+  components: {
+    ButtonFade
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ personal/transition/src/components/ButtonFade.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+
+      <transition name="fade">
+          <p v-if="show">Hello</p>
+      </transition>
+      <button @click="show = !show">Fade Button</button>
+  </div>
+</template>
+
+<script>
+export default {
+    data : function(){
+        return {
+            show : true
+        }
+    }
+}
+</script>
+
+<style>
+    .fade-enter-active, .fade-leave-active{
+        transition: all 1s;
+    }
+    .fade-enter, .fade-leave-to{
+        opacity: 0;
+    }
+</style>

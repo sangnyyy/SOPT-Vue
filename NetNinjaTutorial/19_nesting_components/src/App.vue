b1173a574b3d5368bf47f3e841284e5fb7c6bca7
SML <dltkdals2202@gmail.com>
2018-05-14 15:38:37 +0900

    feat : add a webpack project for tutorial 20, 21
    Tutorial #20 : Nested Components.
    Tutorial #21 : Component CSS.

---
+++ NetNinjaTutorial/19_nesting_components/src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <h1>{{ title }}</h1>
+    <persons></persons>
+  </div>
+</template>
+
+<script>
+
+import Persons from "./components/Persons"
+
+export default {
+  data(){
+    return {
+      title : "ninja app"
+    }
+  },
+  components : {
+    Persons
+  }
+}
+</script>
+
+<style>
+  h1{
+    color : red;
+  }
+</style>
---
+++ NetNinjaTutorial/19_nesting_components/src/components/Persons.vue
@@ -0,0 +1,25 @@
+<template>
+<div>
+    <h1>List of Persons</h1>
+    <ul>
+        <li v-for="person in persons">{{ person }}</li>
+    </ul>
+</div>
+    
+</template>
+
+<script>
+export default {
+    data : function(){
+        return {
+            persons : ['lee', 'wow', 'hello']
+        }
+    }
+}
+</script>
+
+<style scoped>
+    h1{
+        color : green;
+    }
+</style>

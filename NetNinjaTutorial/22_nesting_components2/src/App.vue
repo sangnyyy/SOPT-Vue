46255ebff86e68ca75c0741713dae447bd51cc75
SML <dltkdals2202@gmail.com>
2018-05-14 16:01:33 +0900

    feat : add a webpack Vue project for tutorial 22
    Tutorial #22 : deepen nesting components.

---
+++ NetNinjaTutorial/22_nesting_components2/src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <app-header></app-header>
+    <ninjas></ninjas>
+    <app-footer></app-footer>
+  </div>
+  
+</template>
+
+<script>
+import Header from "./components/Header"
+import Footer from "./components/Footer"
+import Ninjas from "./components/Ninjas"
+export default {
+  components: {
+    'app-header' : Header,
+    'app-footer' : Footer,
+    'ninjas' : Ninjas
+  }
+}
+</script>
+
+<style>
+
+</style>

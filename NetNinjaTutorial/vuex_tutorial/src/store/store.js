6ef92cd1f6a232efc8783e4c8967f224c7b97eb2
SML <dltkdals2202@gmail.com>
2018-05-18 01:56:09 +0900

    feat: add computed to components
    add computed to components and access to the store at store.js using by vuex

--- NetNinjaTutorial/vuex_tutorial/src/store/store.js
+++ NetNinjaTutorial/vuex_tutorial/src/store/store.js
@@ -1,5 +1,5 @@
-import Vue from 'vue';
-import Vuex from 'vuex';
+import Vue from 'vue'
+import Vuex from 'vuex'
 
 Vue.use(Vuex);
 
@@ -12,4 +12,4 @@ export const store = new Vuex.Store({
             {name:"pear", price:1}
           ]
     }
-})
+});
